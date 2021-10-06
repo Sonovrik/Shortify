023e6c1dfaea782cd902f968a6e57258fd09db2c
Sonovrik <dmc2001@mail.ru>
2021-10-06 20:35:32 +0300

    Added configuration for DB

--- shortify/main.go
+++ shortify/main.go
@@ -1,32 +1,72 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
-func serveFiles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
-	p := "." + r.URL.Path
-	if p == "./" {
-		p = "./html/"
+var db *sql.DB
+const dbConfig =
+	"host=postgress_container " +
+	"port=5432 " +
+	"dbname=postgres_db " +
+	"sslmode=disable " +
+	"user=root " +
+	"password=root"
+
+func	getDB() *sql.DB {
+	if db == nil {
+		var err error
+		db, err = sql.Open("postgres", dbConfig)
+		if err != nil {
+			log.Fatalf("Unable to connect to database: %s", err)
+		}
+		if err = db.Ping(); err != nil {
+			log.Fatalf("Unable to ping to database: %s", err)
+		}
+		fmt.Println("DB OK")
+	}
+	return db
+}
+
+func test(w http.ResponseWriter, r *http.Request){
+
+	db = getDB()
+
+	uri := r.URL.Query().Get("uri")
+	if len(uri) == 0 {
+		w.WriteHeader(http.StatusForbidden)
 	}
 
-	//http.ServeFile(w, r, http.FileServer(http.Dir(p)))
+
+}
+
+func	getEnv(key string) string {
+	env, exists := os.LookupEnv(key)
+	if !exists {
+		return ""
+	}
+	return env
 }
 
 func main() {
-	port, exists := os.LookupEnv("SHORTIFY_CONTAINER_PORT")
-	if !exists {
-		panic("No SHORTIFY_CONTAINER_PORT")
+	port := getEnv("SHORTIFY_CONTAINER_PORT")
+	if len(port) == 0 {
+		panic("Envs not found :(")
 	}
 
-	//fileServer := http.FileServer(http.Dir("./html"))
-	//http.Handle("/html/", http.StripPrefix("/html/", fileServer))
-	http.Handle("/", http.FileServer(http.Dir("./html/")))
+	db = getDB()
 
-	//http.HandleFunc("/", serveFiles)
+
+	defer db.Close()
+
+	http.Handle("/", http.FileServer(http.Dir("./html/")))
+	http.HandleFunc("/asd", test)
+
 	log.Fatal(http.ListenAndServe("0.0.0.0:" + port, nil))
 }
