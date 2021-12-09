93829f54b5fa176f973706adffbc9af800de291e
sonovrik <dmc2001@mail.ru>
2021-12-09 21:26:44 +0300

    Basic middleware

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,8 +1,57 @@
 package server
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"
+
+type JsonRequestUrl struct {
+	LongUrl string `json:"longurl"`
+}
+
+func isValidUrl(token string) bool {
+	_, err := url.ParseRequestURI(token)
+	if err != nil {
+		return false
+	}
+
+	u, err := url.Parse(token)
+	if err != nil || u.Host == "" {
+		return false
+	}
+
+	return true
+}
 
 func (s *HttpService) dataValidation(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			// 500, ctx
+			w.WriteHeader(http.StatusInternalServerError)
+			next.ServeHTTP(w, r)
+		}
+
+		bodyData := JsonRequestUrl{}
+
+		if err = json.Unmarshal(body, &bodyData); err != nil {
+			// error in json body, ctx
+			w.WriteHeader(http.StatusBadRequest)
+			next.ServeHTTP(w, r)
+		}
+
+		if ok := isValidUrl(bodyData.LongUrl); !ok {
+			// wrong url content, ctx
+			w.WriteHeader(http.StatusBadRequest)
+			next.ServeHTTP(w, r)
+		}
+
+		ctx := context.WithValue(r.Context(), "Long url", bodyData.LongUrl)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
---
+++ internal/urlMapping/url.go
@@ -0,0 +1,6 @@
+package urlMapping
+
+type UrlMapping struct {
+	ShortUrl string `json:"shorturl"`
+	LongUrl  string `json:"longurl"`
+}
